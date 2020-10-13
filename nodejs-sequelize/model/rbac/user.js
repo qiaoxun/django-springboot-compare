d2dd028dd979c8c6002a1ca538f788fdaf24f698
Xun Qiao <woqiaoxun@163.com>
2020-10-13 19:48:09 +0800

    2020.10.13

--- nodejs-sequelize/model/rbac/user.js
+++ nodejs-sequelize/model/rbac/user.js
@@ -11,29 +11,6 @@ const {
     Organization
 } = require('./organization')
 
-// const User = sequelize.define('User', {
-//         name: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         },
-//         firstName: {
-//             type: DataTypes.STRING
-//         },
-//         lastName: {
-//             type: DataTypes.STRING
-//         },
-//         password: {
-//             type: DataTypes.STRING
-//         },
-//         email: {
-//             type: DataTypes.STRING
-//         },
-//         organizationId: {
-//             type: DataTypes.INTEGER,
-//         }
-//     }
-// );
-
 class User extends Model {}
 User.init({
     // attributes
@@ -55,21 +32,6 @@ User.init({
     },
     organization_id: {
         type: DataTypes.INTEGER,
-
-        // references: {
-        //     // This is a reference to another model
-        //     model: Organization,
-
-        //     // This is the column name of the referenced model
-        //     key: 'id',
-
-        //     // With PostgreSQL, it is optionally possible to declare when to check the foreign key constraint, passing the Deferrable type.
-        //     deferrable: Deferrable.INITIALLY_IMMEDIATE
-        //     // Options:
-        //     // - `Deferrable.INITIALLY_IMMEDIATE` - Immediately check the foreign key constraints
-        //     // - `Deferrable.INITIALLY_DEFERRED` - Defer all foreign key constraint check to the end of a transaction
-        //     // - `Deferrable.NOT` - Don't defer the checks at all (default) - This won't allow you to dynamically change the rule in a transaction
-        //   }
     }
 }, {
     sequelize,
@@ -77,12 +39,6 @@ User.init({
     tableName: 'user'
     // options
 });
-
-// Organization.associations = function() {
-//     User.belongsTo(Organization, {foreignKey: 'organizationId', targetKey: 'id'})
-// }
-
-// User.belongsTo(Organization, {foreignKey: 'organizationId', targetKey: 'id'})
 
 module.exports = {
     User
